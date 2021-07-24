98fdb272f18ac91157f7e1183307764d9fc23d55
Kamranislam0123 <Kamranislam8@gmail.com>
2021-07-24 12:36:07 +0600

    Update sign-in.php

--- viewer/sign-in.php
+++ viewer/sign-in.php
@@ -4,30 +4,26 @@ session_start();
 if (isset($_SESSION['student_login'])) {
     header('location:index.php');
 }
-if (isset($_POST['login'])){
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $result=mysqli_query($con, query:"SELECT * FROM `students` WHERE `email`='$email' OR `username`='$email';");
-   
-       if(mysqli_num_rows($result)==1){
-        $row = mysqli_fetch_assoc($result);
-        if (password_verify($password,$row['password'])){
-            if ($row['status']==1){
-                $_SESSION['student_login']=$email;
-                header('location:index.php');
+if (isset($_POST['login'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $result = mysqli_query($con, query: "SELECT * FROM `students` WHERE `email`='$email' OR `username`='$email';");
 
-            } else{
-                $error="Your Status inactive";
+    if (mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+        if (password_verify($password, $row['password'])) {
+            if ($row['status'] == 0) {
+                $_SESSION['student_login'] = $email;
+                header('location:index.php');
+            } else {
+                $error = "Your Status inactive";
             }
-        } else{
-            $error="password Invalid";
+        } else {
+            $error = "password Invalid";
         }
-       } else{
-           $error="Email Or Username Invalid";
-       }
-        
-        
-        
+    } else {
+        $error = "Email Or Username Invalid";
+    }
 }
 
 ?>
@@ -38,6 +34,7 @@ if (isset($_POST['login'])){
 
 
 <!-- Mirrored from myiideveloper.com/helsinki/last-version/helsinki_green-dark/src/pages_sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 05 Mar 2019 13:05:33 GMT -->
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
@@ -54,78 +51,81 @@ if (isset($_POST['login'])){
 </head>
 
 <body>
-<div class="wrap">
-    <!-- page BODY -->
-    <!-- ========================================================= -->
-    <div class="page-body animated slideInDown">
-        <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
-        <!--LOGO-->
-        <h2 class="text-center" > Library Student  Sign In </h2>
-        <?php
-        if (isset($error)){ 
-        ?>
-        <div class="alert alert-danger" role="alert">
-  <?=$error ?>
-  <button type="button" class="close" data-dismiss="alert" aria-level="close">
-  <span aria-hidden="true">&times;</span>
-  </button> 
-</div>
-<?php 
-        }
-        ?>
-        <div class="box">
-            <!--SIGN IN FORM-->
-            <div class="panel mb-none">
-                <div class="panel-content bg-scale-0">
-                    <form method="post" action="<?=$_SERVER['PHP_SELF']?>">
-                        <div class="form-group mt-md">
-                            <span class="input-with-icon">
-                                <input type="text" class="form-control"  name="email" id="email" placeholder="Email" value="<?=isset($email) ? $email:'' ?>">
-                                <i class="fa fa-envelope"></i>
-                            </span>
-                        </div>
-                        <div class="form-group">
-                            <span class="input-with-icon">
-                                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
-                                <i class="fa fa-key"></i>
-                            </span>
-                        </div>
-                        <div class="form-group">
-                            <div class="checkbox-custom checkbox-primary">
-                                <input type="checkbox" id="remember-me" value="option1" checked>
-                                <label class="check" for="remember-me">Remember me</label>
+    <div class="wrap">
+        <!-- page BODY -->
+        <!-- ========================================================= -->
+        <div class="page-body animated slideInDown">
+            <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
+            <!--LOGO-->
+            <div class="logo">
+                <h2 class="text-center"> Library Student log In </h2>
+                <?php
+                if (isset($error)) {
+                ?>
+                    <div class="alert alert-danger" role="alert">
+                        <?= $error ?>
+                        <button type="button" class="close" data-dismiss="alert" aria-level="close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+            <div class="box">
+                <!--SIGN IN FORM-->
+                <div class="panel mb-none">
+                    <div class="panel-content bg-scale-0">
+                        <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>">
+                            <div class="form-group mt-md">
+                                <span class="input-with-icon">
+                                    <input type="text" class="form-control" name="email" id="email" placeholder="Email" value="<?= isset($email) ? $email : '' ?>">
+                                    <i class="fa fa-envelope"></i>
+                                </span>
                             </div>
-                        </div>
-                        <div class="form-group text-center">
-                        <input type="submit" value=" Sign in" class=" btn btn-primary btn block" name="login">
-                           
-                        </div>
-                        <div class="form-group text-center">
-                            <a href="pages_forgot-password.html">Forgot password?</a>
-                            <hr/>
-                             <span>Don't have an account?</span>
-                            <a href="register.php" class="btn btn-block mt-sm">Register</a>
-                        </div>
-                    </form>
+                            <div class="form-group">
+                                <span class="input-with-icon">
+                                    <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                                    <i class="fa fa-key"></i>
+                                </span>
+                            </div>
+                            <div class="form-group">
+                                <div class="checkbox-custom checkbox-primary">
+                                    <input type="checkbox" id="remember-me" value="option1" checked>
+                                    <label class="check" for="remember-me">Remember me</label>
+                                </div>
+                            </div>
+                            <div class="form-group text-center">
+                                <input type="submit" value=" Log in" class=" btn btn-primary btn block" name="login">
+
+                            </div>
+                            <div class="form-group text-center">
+                                <a href="pages_forgot-password.html">Forgot password?</a>
+                                <hr />
+                                <span>Don't have an account?</span>
+                                <a href="register.php" class="btn btn-block mt-sm">Register</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
+            <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
         </div>
-        <!-- =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= -->
     </div>
-</div>
-<!--BASIC scripts-->
-<!-- ========================================================= -->
-<script src="../asset/vendor/jquery/jquery-1.12.3.min.js"></script>
-<script src="../asset/vendor/bootstrap/js/bootstrap.min.js"></script>
-<script src="../asset/vendor/nano-scroller/nano-scroller.js"></script>
-<!--TEMPLATE scripts-->
-<!-- ========================================================= -->
-<script src="../asset/javascripts/template-script.min.js"></script>
-<script src="../asset/javascripts/template-init.min.js"></script>
-<!-- SECTION script and examples-->
-<!-- ========================================================= -->
+    <!--BASIC scripts-->
+    <!-- ========================================================= -->
+    <script src="../asset/vendor/jquery/jquery-1.12.3.min.js"></script>
+    <script src="../asset/vendor/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../asset/vendor/nano-scroller/nano-scroller.js"></script>
+    <!--TEMPLATE scripts-->
+    <!-- ========================================================= -->
+    <script src="../asset/javascripts/template-script.min.js"></script>
+    <script src="../asset/javascripts/template-init.min.js"></script>
+    <!-- SECTION script and examples-->
+    <!-- ========================================================= -->
 </body>
 
 
 <!-- Mirrored from myiideveloper.com/helsinki/last-version/helsinki_green-dark/src/pages_sign-in.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 05 Mar 2019 13:05:37 GMT -->
+
 </html>
